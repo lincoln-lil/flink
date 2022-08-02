fc3d59f04d351f7312835ed10e4a2c661f95118c
lincoln.lil <lincoln.86xy@gmail.com>
2022-08-02 15:33:02 +0800

    [FLINK-28570][table-planner] Optimize upsertKey inference for lookup join

--- flink-table/flink-table-planner/src/main/scala/org/apache/flink/table/planner/plan/metadata/FlinkRelMdUpsertKeys.scala
+++ flink-table/flink-table-planner/src/main/scala/org/apache/flink/table/planner/plan/metadata/FlinkRelMdUpsertKeys.scala
@@ -24,6 +24,7 @@ import org.apache.flink.table.planner.plan.nodes.physical.batch.{BatchPhysicalGr
 import org.apache.flink.table.planner.plan.nodes.physical.common.CommonPhysicalLookupJoin
 import org.apache.flink.table.planner.plan.nodes.physical.stream.{StreamPhysicalChangelogNormalize, StreamPhysicalDeduplicate, StreamPhysicalDropUpdateBefore, StreamPhysicalGlobalGroupAggregate, StreamPhysicalGroupAggregate, StreamPhysicalGroupWindowAggregate, StreamPhysicalIntervalJoin, StreamPhysicalLocalGroupAggregate, StreamPhysicalOverAggregate}
 import org.apache.flink.table.planner.plan.schema.IntermediateRelTable
+import org.apache.flink.table.planner.plan.utils.FlinkRexUtil
 
 import com.google.common.collect.ImmutableSet
 import org.apache.calcite.plan.hep.HepRelVertex
@@ -228,17 +229,29 @@ class FlinkRelMdUpsertKeys private extends MetadataHandler[UpsertKeys] {
       join: CommonPhysicalLookupJoin,
       mq: RelMetadataQuery): util.Set[ImmutableBitSet] = {
     val left = join.getInput
-    val leftKeys = FlinkRelMetadataQuery.reuseOrCreate(mq).getUpsertKeys(left)
     val leftType = left.getRowType
     val leftJoinKeys = join.joinInfo.leftSet
+    // differs from regular join, here we do not filterKeys because there's no shuffle on join keys
+    // by default.
+    val leftUpsertKeys = FlinkRelMetadataQuery.reuseOrCreate(mq).getUpsertKeys(left)
+    val rightUniqueKeys = FlinkRelMdUniqueKeys.INSTANCE.getUniqueKeysOfTemporalTable(join)
+
+    val rightUpsertKeys =
+      if (
+        (join.remainingCondition.isDefined && !FlinkRexUtil.isDeterministicInStreaming(
+          join.remainingCondition.get))
+        || (join.calcOnTemporalTable.isDefined && !FlinkRexUtil.isDeterministicInStreaming(
+          join.calcOnTemporalTable.get))
+      ) { null }
+      else { rightUniqueKeys }
+
     FlinkRelMdUniqueKeys.INSTANCE.getJoinUniqueKeys(
       join.joinType,
       leftType,
-      filterKeys(leftKeys, leftJoinKeys),
-      null,
-      areColumnsUpsertKeys(leftKeys, leftJoinKeys),
-      // TODO get uniqueKeys from TableSchema of TableSource
-      null
+      leftUpsertKeys,
+      rightUpsertKeys,
+      areColumnsUpsertKeys(leftUpsertKeys, leftJoinKeys),
+      rightUpsertKeys != null
     )
   }
 
--- flink-table/flink-table-planner/src/main/scala/org/apache/flink/table/planner/plan/nodes/physical/common/CommonPhysicalLookupJoin.scala
+++ flink-table/flink-table-planner/src/main/scala/org/apache/flink/table/planner/plan/nodes/physical/common/CommonPhysicalLookupJoin.scala
@@ -17,12 +17,12 @@
  */
 package org.apache.flink.table.planner.plan.nodes.physical.common
 
-import org.apache.flink.table.api.TableException
+import org.apache.flink.table.api.{TableException, TableSchema}
 import org.apache.flink.table.catalog.ObjectIdentifier
 import org.apache.flink.table.functions.{AsyncTableFunction, TableFunction, UserDefinedFunction}
 import org.apache.flink.table.planner.calcite.FlinkTypeFactory
 import org.apache.flink.table.planner.plan.nodes.FlinkRelNode
-import org.apache.flink.table.planner.plan.schema.{LegacyTableSourceTable, TableSourceTable}
+import org.apache.flink.table.planner.plan.schema.{IntermediateRelTable, LegacyTableSourceTable, TableSourceTable}
 import org.apache.flink.table.planner.plan.utils.{ExpressionFormat, JoinTypeUtil, LookupJoinUtil, RelExplainUtil}
 import org.apache.flink.table.planner.plan.utils.ExpressionFormat.ExpressionFormat
 import org.apache.flink.table.planner.plan.utils.LookupJoinUtil._
@@ -33,13 +33,14 @@ import org.apache.flink.table.runtime.types.PlannerTypeUtils
 import org.apache.calcite.plan.{RelOptCluster, RelOptTable, RelTraitSet}
 import org.apache.calcite.rel.`type`.{RelDataType, RelDataTypeField}
 import org.apache.calcite.rel.{RelNode, RelWriter, SingleRel}
-import org.apache.calcite.rel.core.{JoinInfo, JoinRelType}
+import org.apache.calcite.rel.core.{JoinInfo, JoinRelType, TableScan}
 import org.apache.calcite.rex._
 import org.apache.calcite.sql.{SqlExplainLevel, SqlKind}
 import org.apache.calcite.sql.fun.SqlStdOperatorTable
 import org.apache.calcite.sql.validate.SqlValidatorUtil
 import org.apache.calcite.util.mapping.IntPair
 
+import java.util
 import java.util.Collections
 
 import scala.collection.JavaConverters._
@@ -68,7 +69,7 @@ import scala.collection.mutable
  * records <br> 3) join left input record and lookup-ed records <br> 4) only outputs the rows which
  * match to the remainingCondition <br>
  *
- * @param input
+ * @param inputRel
  *   input rel node
  * @param calcOnTemporalTable
  *   the calc (projection&filter) after table scan before joining
@@ -76,13 +77,13 @@ import scala.collection.mutable
 abstract class CommonPhysicalLookupJoin(
     cluster: RelOptCluster,
     traitSet: RelTraitSet,
-    input: RelNode,
+    inputRel: RelNode,
     // TODO: refactor this into TableSourceTable, once legacy TableSource is removed
-    temporalTable: RelOptTable,
+    val temporalTable: RelOptTable,
     val calcOnTemporalTable: Option[RexProgram],
     val joinInfo: JoinInfo,
     val joinType: JoinRelType)
-  extends SingleRel(cluster, traitSet, input)
+  extends SingleRel(cluster, traitSet, inputRel)
   with FlinkRelNode {
 
   val allLookupKeys: Map[Int, LookupKey] = {
@@ -94,7 +95,7 @@ abstract class CommonPhysicalLookupJoin(
   // remaining condition used to filter the joined records (left input record X lookup-ed records)
   val remainingCondition: Option[RexNode] = getRemainingJoinCondition(
     cluster.getRexBuilder,
-    input.getRowType,
+    inputRel.getRowType,
     calcOnTemporalTable,
     allLookupKeys.values.toList,
     joinInfo)
@@ -114,7 +115,7 @@ abstract class CommonPhysicalLookupJoin(
       temporalTable.getRowType
     }
     SqlValidatorUtil.deriveJoinRowType(
-      input.getRowType,
+      inputRel.getRowType,
       rightType,
       joinType,
       flinkTypeFactory,
@@ -123,7 +124,7 @@ abstract class CommonPhysicalLookupJoin(
   }
 
   override def explainTerms(pw: RelWriter): RelWriter = {
-    val inputFieldNames = input.getRowType.getFieldNames.asScala.toArray
+    val inputFieldNames = inputRel.getRowType.getFieldNames.asScala.toArray
     val tableFieldNames = temporalTable.getRowType.getFieldNames
     val resultFieldNames = getRowType.getFieldNames.asScala.toArray
     val whereString = calcOnTemporalTable match {
@@ -277,6 +278,83 @@ abstract class CommonPhysicalLookupJoin(
     }
     val fieldRefLookupKeys = joinKeyPairs.map(p => (p.target, new FieldRefLookupKey(p.source)))
     constantLookupKeys.toMap[Int, LookupKey] ++ fieldRefLookupKeys.toMap[Int, LookupKey]
+  }
+
+  /** Check if lookup key contains primary key, include constant lookup keys. */
+  def lookupKeyContainsPrimaryKey(): Boolean = {
+    val outputPkIdx = getOutputPrimaryKeyIndexes
+    // use allLookupKeys instead of joinInfo.rightSet because there may exists constant
+    // lookup key(s) which are not included in joinInfo.rightKeys.
+    outputPkIdx.nonEmpty && outputPkIdx.forall(index => allLookupKeys.contains(index))
+  }
+
+  /** Get final output pk indexes if exists, otherwise will get empty. */
+  def getOutputPrimaryKeyIndexes: Array[Int] = {
+    val temporalPkIdxs = getPrimaryKeyIndexesOfTemporalTable
+
+    val outputPkIdx = if (!temporalPkIdxs.isEmpty) {
+      if (calcOnTemporalTable.isDefined) {
+        val program = calcOnTemporalTable.get
+        val outputMapping = program.getProjectList.asScala.zipWithIndex
+          .map { case (ref, index) => (index, program.expandLocalRef(ref)) }
+          .map {
+            case (outIndex, ref) =>
+              ref match {
+                case inputRef: RexInputRef => (inputRef.getIndex, outIndex)
+                case _ => (-1, -1)
+              }
+          }
+          .toMap
+        val outputPk = temporalPkIdxs.forall(outputMapping.contains)
+        if (outputPk) {
+          // remapping pk index
+          temporalPkIdxs.map(outputMapping.get(_).get)
+        } else {
+          Array[Int]()
+        }
+      } else {
+        temporalPkIdxs
+      }
+    } else {
+      // temporal table has no pk, no uk produces
+      Array[Int]()
+    }
+
+    outputPkIdx
+  }
+
+  private def getPrimaryKeyIndexesOfTemporalTable: Array[Int] = {
+    // get primary key columns of lookup table if exists
+    val pkColumns = getPrimaryKeyColumnsOfTemporalTable
+    if (pkColumns.isDefined) {
+      val newSchema = temporalTable.getRowType.getFieldNames
+      pkColumns.get.toArray().map(col => newSchema.indexOf(col))
+    } else {
+      Array[Int]()
+    }
+  }
+
+  private def getPrimaryKeyColumnsOfTemporalTable: Option[util.List[String]] = {
+    val schema = temporalTable match {
+      case t: TableSourceTable =>
+        TableSchema.fromResolvedSchema(t.contextResolvedTable.getResolvedSchema)
+      case t: LegacyTableSourceTable[_] => t.catalogTable.getSchema
+      case t: IntermediateRelTable =>
+        t.relNode match {
+          case scan: TableScan =>
+            TableSchema.fromResolvedSchema(
+              scan.getTable.asInstanceOf[TableSourceTable].contextResolvedTable.getResolvedSchema)
+          case _ =>
+            throw new TableException(
+              "Unexpected exception: the node inside intermediate table must be a table source scan")
+        }
+    }
+    val constraint = schema.getPrimaryKey
+    if (constraint.isPresent) {
+      Option(constraint.get().getColumns)
+    } else {
+      Option.empty
+    }
   }
 
   // ----------------------------------------------------------------------------------------
