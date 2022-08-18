dd35c162e425803db3ec727718afb6c34b089651
lincoln lee <lincoln.86xy@gmail.com>
2022-08-18 13:41:55 +0800

    [FLINK-28987][table-planner] Fix incorrect async to sync lookup fallback path of LegacyTableSourceTable
    by fixing this we can completely avoid create user lookup function instance to determine if async lookup
    is enabled for execution, and make the exec lookup join node immutable and easier to maintain

--- flink-table/flink-table-planner/src/main/scala/org/apache/flink/table/planner/plan/nodes/physical/common/CommonPhysicalLookupJoin.scala
+++ flink-table/flink-table-planner/src/main/scala/org/apache/flink/table/planner/plan/nodes/physical/common/CommonPhysicalLookupJoin.scala
@@ -19,21 +19,24 @@ package org.apache.flink.table.planner.plan.nodes.physical.common
 
 import org.apache.flink.table.api.TableException
 import org.apache.flink.table.catalog.{ObjectIdentifier, UniqueConstraint}
+import org.apache.flink.table.connector.ChangelogMode
 import org.apache.flink.table.planner.calcite.FlinkTypeFactory
 import org.apache.flink.table.planner.plan.nodes.FlinkRelNode
-import org.apache.flink.table.planner.plan.nodes.exec.spec.LookupJoinHintSpec
+import org.apache.flink.table.planner.plan.nodes.physical.stream.StreamPhysicalRel
 import org.apache.flink.table.planner.plan.schema.{IntermediateRelTable, LegacyTableSourceTable, TableSourceTable}
-import org.apache.flink.table.planner.plan.utils.{ExpressionFormat, JoinTypeUtil, LookupJoinUtil, RelExplainUtil}
+import org.apache.flink.table.planner.plan.utils.{ChangelogPlanUtils, ExpressionFormat, JoinTypeUtil, LookupJoinUtil, RelExplainUtil}
 import org.apache.flink.table.planner.plan.utils.ExpressionFormat.ExpressionFormat
 import org.apache.flink.table.planner.plan.utils.LookupJoinUtil._
 import org.apache.flink.table.planner.plan.utils.PythonUtil.containsPythonCall
 import org.apache.flink.table.planner.plan.utils.RelExplainUtil.preferExpressionFormat
+import org.apache.flink.table.planner.utils.ShortcutUtils.unwrapTableConfig
 import org.apache.flink.table.runtime.types.PlannerTypeUtils
 
 import org.apache.calcite.plan.{RelOptCluster, RelOptTable, RelTraitSet}
 import org.apache.calcite.rel.`type`.{RelDataType, RelDataTypeField}
 import org.apache.calcite.rel.{RelNode, RelWriter, SingleRel}
 import org.apache.calcite.rel.core.{JoinInfo, JoinRelType, TableScan}
+import org.apache.calcite.rel.hint.RelHint
 import org.apache.calcite.rex._
 import org.apache.calcite.sql.{SqlExplainLevel, SqlKind}
 import org.apache.calcite.sql.fun.SqlStdOperatorTable
@@ -83,7 +86,8 @@ abstract class CommonPhysicalLookupJoin(
     val calcOnTemporalTable: Option[RexProgram],
     val joinInfo: JoinInfo,
     val joinType: JoinRelType,
-    val lookupHintSpec: Option[LookupJoinHintSpec] = Option.empty[LookupJoinHintSpec])
+    val lookupHint: Option[RelHint] = Option.empty[RelHint],
+    val upsertMaterialize: Boolean = false)
   extends SingleRel(cluster, traitSet, inputRel)
   with FlinkRelNode {
 
@@ -106,6 +110,31 @@ abstract class CommonPhysicalLookupJoin(
       "Only inner join condition with equality predicates supports the " +
         "Python UDF taking the inputs from the left table and the right table at the same time, " +
         "e.g., ON T1.id = T2.id && pythonUdf(T1.a, T2.b)")
+  }
+
+  val isAsyncEnabled = LookupJoinUtil.isAsyncLookup(
+    temporalTable,
+    allLookupKeys.keys.map(Int.box).toList.asJava,
+    lookupHint.orNull,
+    upsertMaterialize)
+
+  val retryOptions = Option.apply(LookupJoinUtil.RetryLookupOptions.fromJoinHint(lookupHint.orNull))
+
+  val inputChangelogMode = getInput match {
+    case streamPhysicalRel: StreamPhysicalRel =>
+      ChangelogPlanUtils.getChangelogMode(streamPhysicalRel).getOrElse(ChangelogMode.insertOnly())
+    case _ => ChangelogMode.insertOnly()
+  }
+
+  val tableConfig = unwrapTableConfig(this);
+
+  val asyncOptions = isAsyncEnabled match {
+    case true =>
+      Option.apply(
+        LookupJoinUtil.getMergedAsyncOptions(lookupHint.orNull, tableConfig, inputChangelogMode))
+    case false =>
+      // do not create asyncOptions if async is not enabled
+      Option.empty[AsyncLookupOptions]
   }
 
   override def deriveRowType(): RelDataType = {
@@ -161,14 +190,6 @@ abstract class CommonPhysicalLookupJoin(
       case t: LegacyTableSourceTable[_] => t.tableIdentifier
     }
 
-    // The lookup function maybe not the final choice at runtime because lack of upsert materialize
-    // info here. This can be consistent after planner offers enough info here.
-    val isAsyncEnabled: Boolean =
-      LookupJoinUtil.isAsyncLookup(
-        temporalTable,
-        allLookupKeys.keys.map(Int.box).toList.asJava,
-        lookupHintSpec.orNull)
-
     super
       .explainTerms(pw)
       .item("table", tableIdentifier.asSummaryString())
@@ -185,6 +206,7 @@ abstract class CommonPhysicalLookupJoin(
           pw.getDetailLevel),
         remainingCondition.isDefined)
       .item("select", selection)
+      .itemIf("upsertMaterialize", "true", upsertMaterialize)
   }
 
   /** Gets the remaining join condition which is used */
---
+++ flink-table/flink-table-planner/src/test/java/org/apache/flink/table/planner/plan/nodes/exec/serde/AsyncLookupOptionsTest.java
@@ -0,0 +1,101 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.table.planner.plan.nodes.exec.serde;
+
+import org.apache.flink.streaming.api.datastream.AsyncDataStream;
+import org.apache.flink.table.api.TableConfig;
+import org.apache.flink.table.api.config.ExecutionConfigOptions;
+import org.apache.flink.table.connector.ChangelogMode;
+import org.apache.flink.table.planner.plan.nodes.exec.spec.LookupJoinHintTestUtil;
+import org.apache.flink.table.planner.plan.utils.LookupJoinUtil;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.apache.flink.table.planner.plan.nodes.exec.serde.JsonSerdeTestUtil.testJsonRoundTrip;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/** Tests for {@link LookupJoinUtil.AsyncLookupOptions}. */
+public class AsyncLookupOptionsTest {
+
+    @Test
+    void testSerdeAsyncLookupOptions() throws IOException {
+        LookupJoinUtil.AsyncLookupOptions asyncLookupOptions =
+                LookupJoinUtil.getMergedAsyncOptions(
+                        LookupJoinHintTestUtil.completeLookupHint,
+                        TableConfig.getDefault(),
+                        ChangelogMode.insertOnly());
+        testJsonRoundTrip(asyncLookupOptions, LookupJoinUtil.AsyncLookupOptions.class);
+
+        asyncLookupOptions =
+                LookupJoinUtil.getMergedAsyncOptions(
+                        LookupJoinHintTestUtil.lookupHintWithAsync,
+                        TableConfig.getDefault(),
+                        ChangelogMode.insertOnly());
+        testJsonRoundTrip(asyncLookupOptions, LookupJoinUtil.AsyncLookupOptions.class);
+
+        asyncLookupOptions =
+                LookupJoinUtil.getMergedAsyncOptions(
+                        LookupJoinHintTestUtil.lookupHintWithRetry,
+                        TableConfig.getDefault(),
+                        ChangelogMode.insertOnly());
+        testJsonRoundTrip(asyncLookupOptions, LookupJoinUtil.AsyncLookupOptions.class);
+
+        asyncLookupOptions =
+                LookupJoinUtil.getMergedAsyncOptions(
+                        LookupJoinHintTestUtil.lookupHintWithTableOnly,
+                        TableConfig.getDefault(),
+                        ChangelogMode.insertOnly());
+        testJsonRoundTrip(asyncLookupOptions, LookupJoinUtil.AsyncLookupOptions.class);
+    }
+
+    @Test
+    void testAsyncLookupOptions() {
+        LookupJoinUtil.AsyncLookupOptions asyncLookupOptions =
+                LookupJoinUtil.getMergedAsyncOptions(
+                        LookupJoinHintTestUtil.completeLookupHint,
+                        TableConfig.getDefault(),
+                        ChangelogMode.insertOnly());
+        assertTrue(asyncLookupOptions.asyncOutputMode == AsyncDataStream.OutputMode.UNORDERED);
+
+        asyncLookupOptions =
+                LookupJoinUtil.getMergedAsyncOptions(
+                        LookupJoinHintTestUtil.completeLookupHint,
+                        TableConfig.getDefault(),
+                        ChangelogMode.all());
+        assertTrue(asyncLookupOptions.asyncOutputMode == AsyncDataStream.OutputMode.ORDERED);
+
+        asyncLookupOptions =
+                LookupJoinUtil.getMergedAsyncOptions(
+                        LookupJoinHintTestUtil.lookupHintWithTableOnly,
+                        TableConfig.getDefault(),
+                        ChangelogMode.insertOnly());
+        assertTrue(asyncLookupOptions.asyncOutputMode == AsyncDataStream.OutputMode.ORDERED);
+        assertTrue(
+                asyncLookupOptions.asyncTimeout
+                        == ExecutionConfigOptions.TABLE_EXEC_ASYNC_LOOKUP_TIMEOUT
+                                .defaultValue()
+                                .toMillis());
+        assertTrue(
+                asyncLookupOptions.asyncBufferCapacity
+                        == ExecutionConfigOptions.TABLE_EXEC_ASYNC_LOOKUP_BUFFER_CAPACITY
+                                .defaultValue());
+    }
+}
